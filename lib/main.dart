d770915f2aa3921cc22d75a84050682d1bf2a57b
brynne0 <brynne.obrien@me.com>
2024-08-12 17:57:59 +1000

    Started implementing page 1. Updated red button to have a centered parameter. When set to true, the red button is centered on the screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'color.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -12,17 +11,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-   
       create: (context) => MyAppState(),
-      
       child: MaterialApp(
         title: 'medi_kit',
         theme: ThemeData(
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(seedColor: AppColors.cream),
-          scaffoldBackgroundColor: AppColors.cream, 
+          scaffoldBackgroundColor: AppColors.cream,
         ),
-        home: MyHomePage(),
+        home: const MyHomePage(),
       ),
     );
   }
@@ -30,7 +27,7 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var currentPageNum = 0;
-  
+
   var pages = <Widget>[];
 
   MyAppState() {
@@ -41,40 +38,87 @@ class MyAppState extends ChangeNotifier {
     // Initialize your pages here
     pages = [
       // insert page 1 here
-
+      const Page1(),
       // page 2
-      Page2()
+      const Page2()
       // Add more pages as needed
     ];
     notifyListeners();
   }
 
-  void incrementPageNum(){
-    currentPageNum ++;
+  void incrementPageNum() {
+    currentPageNum++;
     notifyListeners();
   }
 
-  void decrementPageNum(){
-    currentPageNum --;
+  void decrementPageNum() {
+    currentPageNum--;
     notifyListeners();
   }
 
-  void setPageNum(pageNumber){
+  void setPageNum(pageNumber) {
     currentPageNum = pageNumber;
     notifyListeners();
   }
 }
 
 class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
     // final theme = Theme.of(context);
 
     return Scaffold(
-      body:  appState.pages[appState.currentPageNum],
+      body: appState.pages[appState.currentPageNum],
     );
-  
+  }
+}
+
+class Page1 extends StatelessWidget {
+  const Page1({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.only(top: 115, left: 65, right: 75),
+      child: Column(
+        children: [
+          Text(
+            "Healthy Connections",
+            style: TextStyle(
+              fontSize: 50,
+              fontWeight: FontWeight.bold,
+              color: AppColors.turquoise,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          Text(
+            "Medi-Kit",
+            style: TextStyle(
+              fontSize: 70,
+              fontWeight: FontWeight.bold,
+              color: AppColors.black,
+            ),
+          ),
+          SizedBox(
+            height: 70,
+          ),
+          FittedBox(child: RowOfButtons()),
+          SizedBox(
+            height: 70,
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: RedActionButton(centered: true, label: "Sign In"),
+          ),
+          // minimumSize: WidgetStateProperty.all<Size>(Size(95, 95)), // Width, Height
+        ],
+      ),
+    );
   }
 }
 
@@ -83,17 +127,11 @@ class Page2 extends StatelessWidget {
     super.key,
   });
 
-
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(
-        top: 50,
-        left:65,
-        right: 75
-    
-      ),
-      child:Column(
+    return const Padding(
+      padding: EdgeInsets.only(top: 50, left: 65, right: 75),
+      child: Column(
         children: [
           BackArrowWelcomeRow(),
           Text(
@@ -103,18 +141,19 @@ class Page2 extends StatelessWidget {
               fontWeight: FontWeight.bold,
             ),
           ),
-          SizedBox(height: 70,),
+          SizedBox(
+            height: 70,
+          ),
           FittedBox(child: RowOfButtons()),
-          SizedBox(height: 70,),
+          SizedBox(
+            height: 70,
+          ),
           Align(
-            alignment:Alignment.bottomRight,
-            child: RedActionButton(iconData: Icons.check_circle,label: "Submit"),
-            
+            alignment: Alignment.bottomRight,
+            child:
+                RedActionButton(iconData: Icons.check_circle, label: "Submit"),
           ),
           // minimumSize: WidgetStateProperty.all<Size>(Size(95, 95)), // Width, Height
-        
-        
-      
         ],
       ),
     );
@@ -125,29 +164,35 @@ class RedActionButton extends StatelessWidget {
   const RedActionButton({
     super.key,
     this.iconData,
+    this.centered = false,
     required this.label,
   });
 
   final IconData? iconData;
   final String label;
+  final bool centered;
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton.icon(
+    Widget button = ElevatedButton.icon(
       onPressed: () {
         print("Submit button pressed, go to next page");
       },
-      icon: iconData != null ? Icon(iconData) : SizedBox.shrink(), // Conditionally show the icon
+      icon: iconData != null
+          ? Icon(iconData)
+          : const SizedBox.shrink(), // Conditionally show the icon
       label: Text(
         label,
-        style: TextStyle(color: Colors.white, fontSize: 20),
+        style: const TextStyle(color: Colors.white, fontSize: 20),
       ),
       style: ButtonStyle(
         iconColor: WidgetStateProperty.all(Colors.white),
         backgroundColor: WidgetStateProperty.all(AppColors.red),
-        minimumSize: WidgetStateProperty.all<Size>(Size(200, 80)),
+        minimumSize: WidgetStateProperty.all<Size>(const Size(200, 80)),
       ),
     );
+
+    return centered ? Center(child: button) : button;
   }
 }
 
@@ -158,21 +203,14 @@ class BackArrowWelcomeRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(
-       
-        bottom:35,
- 
+    return const Padding(
+      padding: EdgeInsets.only(
+        bottom: 35,
       ),
-      child: Row(
-        children:[
-          
-          BackArrow(),
-          WelcomeColumn(),
-    
-       
-        ]
-      ),
+      child: Row(children: [
+        BackArrow(),
+        WelcomeColumn(),
+      ]),
     );
   }
 }
@@ -185,22 +223,20 @@ class WelcomeColumn extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.start ,
+      crossAxisAlignment: CrossAxisAlignment.start,
       mainAxisSize: MainAxisSize.min,
-      children: [                    
-        Padding(
-          padding: const EdgeInsets.only(left: 85),
+      children: [
+        const Padding(
+          padding: EdgeInsets.only(left: 85),
           child: ColoredWelcome(),
-        ),               
+        ),
         Transform.translate(
-          offset:  Offset(0, -20),
-          child: Text(
-            'Karratanyju Olivia!',
-            style: TextStyle(
-              fontSize: 60,
-              fontWeight: FontWeight.bold,
-            )          
-          ),
+          offset: const Offset(0, -20),
+          child: const Text('Karratanyju Olivia!',
+              style: TextStyle(
+                fontSize: 60,
+                fontWeight: FontWeight.bold,
+              )),
         ),
       ],
     );
@@ -224,14 +260,15 @@ class BackArrow extends StatelessWidget {
       child: Container(
         decoration: BoxDecoration(
           shape: BoxShape.circle,
-          border: Border.all(color: Color.fromRGBO(15, 13, 11, 1.0), width: 2.5), 
+          border: Border.all(
+              color: const Color.fromRGBO(15, 13, 11, 1.0), width: 2.5),
         ),
         child: IconButton(
           onPressed: () {
             // appState.placeHolde();
           },
-          color: Color.fromRGBO(15, 13, 11, 1.0),
-          icon: Icon(Icons.arrow_back),
+          color: const Color.fromRGBO(15, 13, 11, 1.0),
+          icon: const Icon(Icons.arrow_back),
         ),
       ),
     );
@@ -239,67 +276,66 @@ class BackArrow extends StatelessWidget {
 }
 
 class ColoredWelcome extends StatelessWidget {
-  
+  const ColoredWelcome({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        RichText(
-          text: TextSpan(
-              text: 'Welcome',
-              style: TextStyle(
-              fontSize: 40,
-              fontWeight: FontWeight.normal,
-              foreground: Paint()
-                ..style = PaintingStyle.stroke
-                ..strokeWidth = 3
-                ..color = Colors.black,
+    return Stack(children: [
+      RichText(
+        text: TextSpan(
+          text: 'Welcome',
+          style: TextStyle(
+            fontSize: 40,
+            fontWeight: FontWeight.normal,
+            foreground: Paint()
+              ..style = PaintingStyle.stroke
+              ..strokeWidth = 3
+              ..color = Colors.black,
+          ),
+        ),
+      ),
+      RichText(
+        text: const TextSpan(
+          children: <TextSpan>[
+            TextSpan(
+              text: 'W',
+              style: TextStyle(color: AppColors.red),
             ),
-          ),
+            TextSpan(
+              text: 'e',
+              style: TextStyle(color: AppColors.green),
+            ),
+            TextSpan(
+              text: 'l',
+              style: TextStyle(color: AppColors.orange),
+            ),
+            TextSpan(
+              text: 'c',
+              style: TextStyle(color: AppColors.blue),
+            ),
+            TextSpan(
+              text: 'o',
+              style: TextStyle(color: AppColors.red),
+            ),
+            TextSpan(
+              text: 'm',
+              style: TextStyle(color: AppColors.green),
+            ),
+            TextSpan(
+              text: 'e',
+              style: TextStyle(color: AppColors.orange),
+            ),
+          ],
+          style: TextStyle(fontSize: 40, fontWeight: FontWeight.normal),
         ),
-        RichText(
-          text: TextSpan(
-            children: <TextSpan>[
-              TextSpan(
-                text: 'W',
-                style: TextStyle(color: AppColors.red),
-              ),
-              TextSpan(
-                text: 'e',
-                style: TextStyle(color: AppColors.green),
-              ),
-              TextSpan(
-                text: 'l',
-                style: TextStyle(color: AppColors.orange),
-              ),
-              TextSpan(
-                text: 'c',
-                style: TextStyle(color: AppColors.blue),
-              ),
-              TextSpan(
-                text: 'o',
-                style: TextStyle(color: AppColors.red),
-              ),
-              TextSpan(
-                text: 'm',
-                style: TextStyle(color: AppColors.green),
-              ),
-              TextSpan(
-                text: 'e',
-                style: TextStyle(color: AppColors.orange),
-              ),
-            ],
-            style: TextStyle(fontSize:40 ,fontWeight: FontWeight.normal),
-          ),
-        ),
-
-      ]
-    );
+      ),
+    ]);
   }
 }
 
-
 class RowOfButtons extends StatelessWidget {
+  const RowOfButtons({super.key});
+
   @override
   Widget build(BuildContext context) {
     List<Widget> buttons = []; // Create an empty list of widgets
@@ -307,10 +343,8 @@ class RowOfButtons extends StatelessWidget {
     for (int i = 0; i < 5; i++) {
       buttons.add(
         Container(
-       
-          decoration: BoxDecoration(
+          decoration: const BoxDecoration(
             shape: BoxShape.circle,
-            
           ),
           child: TextButton(
             onPressed: () {
@@ -319,34 +353,31 @@ class RowOfButtons extends StatelessWidget {
             style: ButtonStyle(
               // to remove the hover effect
               overlayColor: WidgetStateProperty.all(Colors.transparent),
-              minimumSize: WidgetStateProperty.all<Size>(Size(95, 95)), // Width, Height
-            
+              minimumSize: WidgetStateProperty.all<Size>(
+                  const Size(95, 95)), // Width, Height
             ),
-            
-            child: Text(''),
+            child: const Text(''),
           ),
         ),
       );
     }
 
-    return 
-      Container(
-        width: 660,
-        height: 95,
-    
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/images/background.png'),  // Replace with your image path
-            fit: BoxFit.contain,  // This fits the image to cover the entire container
-          ),
+    return Container(
+      width: 660,
+      height: 95,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(
+              'assets/images/background.png'), // Replace with your image path
+          fit: BoxFit
+              .contain, // This fits the image to cover the entire container
         ),
-        
-        child: Row(
-          
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          mainAxisSize: MainAxisSize.max,
-          children: buttons, // Pass the list to the Row widget
-        ),
-      );
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisSize: MainAxisSize.max,
+        children: buttons, // Pass the list to the Row widget
+      ),
+    );
   }
 }
