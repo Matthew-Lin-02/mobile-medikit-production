f35a6435c810022c3ce9ca4c72b3b68be68074e4
ConorNunn <cdnunn@student.unimelb.edu.au>
2024-08-20 15:33:58 +1000

    Formatted File
    Formatted File through 'format on save' option

--- lib/components/StatusTray.dart
+++ lib/components/StatusTray.dart
@@ -4,7 +4,7 @@ import 'package:comp30022/color.dart';
 
 class StatusTray extends StatelessWidget {
   var iconSize = 50.0;
-  
+
   @override
   Widget build(BuildContext context) {
     return IntrinsicWidth(
@@ -19,17 +19,23 @@ class StatusTray extends StatelessWidget {
             mainAxisSize: MainAxisSize.min,
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              StatusIcon(iconSize: iconSize, image: 'assets/images/wifi-connection.png'),
+              StatusIcon(
+                  iconSize: iconSize,
+                  image: 'assets/images/wifi-connection.png'),
               StatusIcon(iconSize: iconSize, image: 'assets/images/globe.png'),
-              StatusIcon(iconSize: iconSize, image: 'assets/images/person-outline.png'),
-              StatusIcon(iconSize: iconSize, image: 'assets/images/settings.png'),
+              StatusIcon(
+                  iconSize: iconSize,
+                  image: 'assets/images/person-outline.png'),
+              StatusIcon(
+                  iconSize: iconSize, image: 'assets/images/settings.png'),
               StatusIcon(iconSize: iconSize, image: 'assets/images/phone.png'),
               Container(
                 decoration: BoxDecoration(
                   color: Colors.red,
                   borderRadius: BorderRadius.circular(20.0),
                 ),
-                child: StatusIcon(iconSize: iconSize, image: 'assets/images/ambulance.png'),
+                child: StatusIcon(
+                    iconSize: iconSize, image: 'assets/images/ambulance.png'),
               ),
             ],
           ),
@@ -57,11 +63,10 @@ class StatusIcon extends StatelessWidget {
       height: iconSize,
       width: iconSize,
       child: IconButton(
-        onPressed: () {},
-        icon: Image.asset(image),
-        padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 5),
-        style: const ButtonStyle(overlayColor: WidgetStateColor.transparent)
-      ),
+          onPressed: () {},
+          icon: Image.asset(image),
+          padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 5),
+          style: const ButtonStyle(overlayColor: WidgetStateColor.transparent)),
     );
   }
 }
