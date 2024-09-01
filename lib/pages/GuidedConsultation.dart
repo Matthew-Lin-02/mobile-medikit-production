e1548577ae4837fa4c662dfbec2fc236a087ca68
Matthew-Lin-02 <matthewlin02@gmail.com>
2024-09-02 03:48:59 +1000

    feat: implement custom centered modal function. Implement Phone Icon and globe icon from status tray with modal functionality. guided consultation page abstraction fix, can now just initialise pages in the initalise page function in  guidedconsultationstate

--- lib/pages/GuidedConsultation.dart
+++ lib/pages/GuidedConsultation.dart
@@ -19,12 +19,8 @@ class _GuidedConsultationState extends State<GuidedConsultation> {
   @override
   Widget build(BuildContext context) {
     var guidedConsultationState = context.watch<GuidedConsultationState>();
-    return AbstractConsultationPage(
-        title: "Guided Consultation",
-        pageNum: guidedConsultationState
-            .getPageNumber(guidedConsultationState.currentPageIndex),
-        body: guidedConsultationState
-            .pageBodies[guidedConsultationState.currentPageIndex]);
+    return guidedConsultationState
+        .pageBodies[guidedConsultationState.currentPageIndex];
   }
 }
 
@@ -33,15 +29,24 @@ class GuidedConsultationState extends ChangeNotifier {
   var pageBodies = <Widget>[];
 
   GuidedConsultationState() {
-    _initializePageBodies();
+    _initializePages();
   }
 
-  void _initializePageBodies() {
+  void _initializePages() {
     // Initialize your pagebodies here
     pageBodies = [
       // page 1
-      GuidedConsultationBody(),
-      ScreeningTools(),
+      AbstractConsultationPage(
+        title: "Guided Consultation",
+        pageNum: 2,
+        body: GuidedConsultationBody(),
+      ),
+
+      AbstractConsultationPage(
+        title: " ScreeningTools",
+        pageNum: 3,
+        body: ScreeningTools(),
+      )
     ];
     notifyListeners();
   }
