763a512ce81b4174755659df31e8754fa9b62e07
felixmccuaig <felixmccuaig@gmail.com>
2024-09-12 10:19:37 +1000

    Add tabs

--- lib/pages/results/ResultsPage.dart
+++ lib/pages/results/ResultsPage.dart
@@ -1,12 +1,108 @@
+import 'package:comp30022/color.dart';
 import 'package:flutter/material.dart';
 import 'package:comp30022/pages/AbstractConsultationPage.dart';
 
-class Results extends StatelessWidget {
-  const Results({super.key});
+class Results extends StatefulWidget {
+  const Results({Key? key}) : super(key: key);
+
+  @override
+  _ResultsState createState() => _ResultsState();
+}
+
+class _ResultsState extends State<Results> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 4, vsync: this);
+    _tabController.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  Widget _buildTab(String text, int index) {
+    bool isSelected = _tabController.index == index;
+    return Expanded(
+      child: InkWell(
+        onTap: () {
+          _tabController.animateTo(index);
+        },
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 16),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color:
+                isSelected ? AppColors.cream.withOpacity(0.7) : AppColors.cream,
+            border: Border(
+              bottom: BorderSide(
+                color: isSelected ? Colors.black45 : Colors.transparent,
+                width: 3, // Increased width for visibility
+              ),
+            ),
+          ),
+          child: Text(
+            text,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 16, // Larger font for selected tab
+              fontWeight: FontWeight.bold,
+              color: isSelected ? Colors.black : Colors.black54,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return AbstractConsultationPage(
-        title: "Results", pageNum: 4, body: const Center());
+    return DefaultTabController(
+      length: 4,
+      child: AbstractConsultationPage(
+        title: "Results",
+        pageNum: 4,
+        body: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: AppColors.cream,
+                border: Border(
+                  bottom: BorderSide(
+                    color: Colors.grey.shade300,
+                    width: 2,
+                  ),
+                ),
+              ),
+              child: Row(
+                children: [
+                  _buildTab('Observations and Vital Sign', 0),
+                  _buildTab('ECG Results', 1),
+                  _buildTab('Image Analysis Results', 2),
+                  _buildTab('Urinalysis Results', 3),
+                ],
+              ),
+            ),
+            Expanded(
+              child: TabBarView(
+                controller: _tabController,
+                children: [
+                  Center(child: Text('Observations and Vital Sign Content')),
+                  Center(child: Text('ECG Results Content')),
+                  Center(child: Text('Image Analysis Results Content')),
+                  Center(child: Text('Urinalysis Results Content')),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
