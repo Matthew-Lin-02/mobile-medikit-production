de3d22a06ec0f09206beca2a8273297bd30d374f
ConorNunn <cdnunn@student.unimelb.edu.au>
2024-10-08 22:09:22 +1100

    Added Footer and Patient Detail layout

--- lib/pages/results/MedicalReport.dart
+++ lib/pages/results/MedicalReport.dart
@@ -1,8 +1,10 @@
 import 'dart:math';
 
+import 'package:comp30022/color.dart';
 import 'package:comp30022/components/ChatbotButton.dart';
 import 'package:comp30022/components/RedActionButton.dart';
 import 'package:comp30022/components/YellowTextField.dart';
+import 'package:comp30022/font.dart';
 import 'package:comp30022/pages/AbstractConsultationPage.dart';
 import 'package:comp30022/styles.dart';
 import 'package:flutter/material.dart';
@@ -22,7 +24,7 @@ class MedicalReportPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AbstractConsultationPage(
-      title: "Patient Plan",
+      title: "Medical Report",
       pageNum: 8,
       body: medicalReportBody,
     );
@@ -36,140 +38,187 @@ class MedicalReportContent extends StatelessWidget {
   Widget build(BuildContext context) {
     double screenWidth = context.screenWidth;
 
-    return SizedBox.expand(
-        child: FractionallySizedBox(
+    return ListView(
+      children: [
+        FractionallySizedBox(
             widthFactor: 0.9,
-            child: ListView(
-              children: [
-                const Text(
-                    'Below is a generated report of Darlene Pilabar\'s health consultation'),
-                const Text('Patient details'),
-                SizedBox(
-                    width: double.infinity,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        GeneratedField(
-                          fieldName: 'Patient Name',
-                          fieldText: 'Darlene Pilbara',
-                          fieldWidth: screenWidth * 0.2,
-                        ),
-                        GeneratedField(
-                            fieldName: 'Patient ID',
-                            fieldText: '00000001',
-                            fieldWidth: screenWidth * 0.2),
-                        GeneratedField(
-                            fieldName: 'Gender',
-                            fieldText: 'Female',
-                            fieldWidth: screenWidth * 0.2),
-                        GeneratedField(
-                            fieldName: 'Date of birth',
-                            fieldText: '01/01/2001',
-                            fieldWidth: screenWidth * 0.2)
-                      ],
-                    )),
-                SizedBox(
-                    width: double.infinity,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        GeneratedField(
-                            fieldName: 'Medical history',
-                            fieldText: 'Chest pains',
-                            fieldWidth: screenWidth * 0.2),
-                        GeneratedField(
-                            fieldName: 'Allergies',
-                            fieldText: 'No reported allergies',
-                            fieldWidth: screenWidth * 0.2)
-                      ],
-                    )),
-                const Text('Consulting worker details'),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    GeneratedField(
-                        fieldName: 'Consultant Name',
-                        fieldText: 'Olivia',
-                        fieldWidth: screenWidth * 0.2),
-                    GeneratedField(
-                        fieldName: 'Occupation',
-                        fieldText: 'Remote area nurse',
-                        fieldWidth: screenWidth * 0.2),
-                    GeneratedField(
-                        fieldName: 'Worker ID',
-                        fieldText: 'RAN00001',
-                        fieldWidth: screenWidth * 0.2),
-                    GeneratedField(
-                        fieldName: 'Date of consult',
-                        fieldText: '01/03/2024',
-                        fieldWidth: screenWidth * 0.2)
-                  ],
-                ),
-                const Divider(),
-                const Text('Medical testing results'),
-                Container(
-                    child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Container(
-                        child: Column(
-                      children: [
-                        Text('Generate testing results'),
-                        Text(
-                            'View observations, temperature and blood pressure results'),
-                        RedActionButton(label: 'View results')
-                      ],
-                    )),
-                    Container(
-                        child: Column(
-                      children: [
-                        Text('ECG results'),
-                        Text(
-                            'View ECG test results, graph, classification and AI analysis'),
-                        RedActionButton(label: 'View results')
-                      ],
-                    )),
-                    Container(
-                        child: Column(
-                      children: [
-                        Text('Urinalysis results'),
-                        Text('View urinalysis examination results'),
-                        RedActionButton(label: 'View results')
-                      ],
-                    ))
-                  ],
-                )),
-                const Divider(),
-                const Text('Checkup details'),
-                const EntryField(
-                    fieldName: 'Checkup results description',
-                    hintText:
-                        'Enter healthy if there are no persisting issues, otherwise enter persistent previous medical issues or new recognised issues...'),
-                GeneratedField(
-                    fieldName: 'Treatment plan',
-                    fieldText:
-                        'Add a cup of frozen veggies to your meal. Balanced eating helps heal the Heart.\n Have a bushwalk with family, lookout for goanna and emu eggs.',
-                    fieldWidth: screenWidth * 0.2),
-                const EntryField(
-                    fieldName: 'Prescribed medicaiton and instructions of use',
-                    hintText: 'Enter here...'),
-                const EntryField(
-                    fieldName: 'Patient follow up instructions',
-                    hintText:
-                        'Enter any instructions to give to patient or details about follow up appointment...'),
-                Container(
-                    child: Row(
-                  children: [
-                    Text(
-                        'I have read this report and agree with the results. Any incorrect results have been amended and I will be hld responsible for issues arising from this record')
-                  ],
-                )),
-                RedActionButton(
-                  label: 'Submit report and complete checkup',
-                  iconData: Icons.arrow_forward,
-                )
-              ],
-            )));
+            child: Column(children: [
+              const Align(
+                  child: Text(
+                      'Below is a generated report of Darlene Pilabar\'s health consultation',
+                      style: TextStyle(
+                          fontSize: subHeadingFontSize, height: 1.2))),
+              const Align(
+                  child: Text(
+                'Patient details',
+                style: TextStyle(fontSize: largeFontSize, height: 1.2),
+              )),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  GeneratedField(
+                    fieldName: 'Patient Name',
+                    fieldText: 'Darlene Pilbara',
+                    fieldWidth: screenWidth * 0.18,
+                    maxLines: 1,
+                    textAlign: TextAlign.center,
+                  ),
+                  GeneratedField(
+                    fieldName: 'Patient ID',
+                    fieldText: '00000001',
+                    fieldWidth: screenWidth * 0.18,
+                    maxLines: 1,
+                    textAlign: TextAlign.center,
+                  ),
+                  GeneratedField(
+                    fieldName: 'Gender',
+                    fieldText: 'Female',
+                    fieldWidth: screenWidth * 0.18,
+                    maxLines: 1,
+                    textAlign: TextAlign.center,
+                  ),
+                  GeneratedField(
+                    fieldName: 'Date of birth',
+                    fieldText: '01/01/2001',
+                    fieldWidth: screenWidth * 0.18,
+                    maxLines: 1,
+                    textAlign: TextAlign.center,
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  GeneratedField(
+                    fieldName: 'Medical history',
+                    fieldText: 'Chest pains',
+                    fieldWidth: screenWidth * 0.4,
+                    maxLines: 3,
+                  ),
+                  GeneratedField(
+                      fieldName: 'Allergies',
+                      fieldText: 'No reported allergies',
+                      fieldWidth: screenWidth * 0.4,
+                      maxLines: 3)
+                ],
+              ),
+              const Align(
+                  child: Text(
+                'Consulting worker details',
+                style: TextStyle(fontSize: largeFontSize, height: 1.2),
+              )),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  GeneratedField(
+                    fieldName: 'Consultant Name',
+                    fieldText: 'Olivia',
+                    fieldWidth: screenWidth * 0.18,
+                    maxLines: 1,
+                    textAlign: TextAlign.center,
+                  ),
+                  GeneratedField(
+                    fieldName: 'Occupation',
+                    fieldText: 'Remote area nurse',
+                    fieldWidth: screenWidth * 0.18,
+                    maxLines: 1,
+                    textAlign: TextAlign.center,
+                  ),
+                  GeneratedField(
+                    fieldName: 'Worker ID',
+                    fieldText: 'RAN00001',
+                    fieldWidth: screenWidth * 0.18,
+                    maxLines: 1,
+                    textAlign: TextAlign.center,
+                  ),
+                  GeneratedField(
+                    fieldName: 'Date of consult',
+                    fieldText: '01/03/2024',
+                    fieldWidth: screenWidth * 0.18,
+                    maxLines: 1,
+                    textAlign: TextAlign.center,
+                  )
+                ],
+              ),
+              const Divider(),
+              const Align(
+                  child: Text(
+                'Medical testing results',
+                style: TextStyle(fontSize: largeFontSize, height: 1.2),
+              )),
+              Container(
+                  child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                      child: Column(
+                    children: [
+                      Text('Generate testing results'),
+                      Text(
+                          'View observations, temperature and blood pressure results'),
+                      RedActionButton(label: 'View results')
+                    ],
+                  )),
+                  Container(
+                      child: Column(
+                    children: [
+                      Text('ECG results'),
+                      Text(
+                          'View ECG test results, graph, classification and AI analysis'),
+                      RedActionButton(label: 'View results')
+                    ],
+                  )),
+                  Container(
+                      child: Column(
+                    children: [
+                      Text('Urinalysis results'),
+                      Text('View urinalysis examination results'),
+                      RedActionButton(label: 'View results')
+                    ],
+                  ))
+                ],
+              )),
+              const Divider(),
+              const Align(
+                  child: Text(
+                'Checkup details',
+                style: TextStyle(fontSize: largeFontSize, height: 1.2),
+              )),
+              const EntryField(
+                  fieldName: 'Checkup results description',
+                  hintText:
+                      'Enter healthy if there are no persisting issues, otherwise enter persistent previous medical issues or new recognised issues...'),
+              GeneratedField(
+                  fieldName: 'Treatment plan',
+                  fieldText:
+                      'Add a cup of frozen veggies to your meal. Balanced eating helps heal the Heart.\nHave a bushwalk with family, lookout for goanna and emu eggs.',
+                  fieldWidth: screenWidth * 0.9),
+              const EntryField(
+                  fieldName: 'Prescribed medicaiton and instructions of use',
+                  hintText: 'Enter here...'),
+              const EntryField(
+                  fieldName: 'Patient follow up instructions',
+                  hintText:
+                      'Enter any instructions to give to patient or details about follow up appointment...'),
+              Container(
+                  decoration: BoxDecoration(
+                      color: Color(0xFFFFF5E1),
+                      border: Border.all(color: Colors.black),
+                      borderRadius: BorderRadius.circular(5)),
+                  child: Row(
+                    children: [
+                      Text(
+                          'I have read this report and agree with the results. Any incorrect results have been amended and I will be held responsible for issues arising from this record')
+                    ],
+                  )),
+              RedActionButton(
+                label: 'Submit report and complete checkup',
+                iconData: Icons.arrow_forward,
+              )
+            ])),
+        Image(image: AssetImage('assets/images/art/footer-strip.png')),
+      ],
+    );
   }
 }
 
@@ -187,8 +236,12 @@ class EntryField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(fieldName),
+        Text(
+          fieldName,
+          style: const TextStyle(fontSize: biggishFontSize),
+        ),
         YellowTextField(
           hintText: hintText,
           maxLines: maxLines ?? 4,
@@ -198,32 +251,40 @@ class EntryField extends StatelessWidget {
   }
 }
 
+// ignore: must_be_immutable
 class GeneratedField extends StatelessWidget {
-  const GeneratedField(
+  GeneratedField(
       {super.key,
       required this.fieldName,
       required this.fieldText,
       this.maxLines,
-      required this.fieldWidth});
+      required this.fieldWidth,
+      this.textAlign});
 
   final String fieldName;
   final String fieldText;
   final int? maxLines;
   final double fieldWidth;
+  TextAlign? textAlign;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(fieldName),
+        Text(
+          fieldName,
+          style: const TextStyle(fontSize: biggishFontSize),
+        ),
         SizedBox(
             width: fieldWidth,
             child: TextField(
+              textAlign: textAlign ?? TextAlign.start,
+              style: const TextStyle(fontSize: biggishFontSize),
               decoration: InputDecoration(
                   hintText: fieldText,
                   border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8.0),
+                    borderRadius: BorderRadius.circular(15.0),
                     borderSide: const BorderSide(
                       color: Colors.black54,
                     ),
